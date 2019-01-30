5a64f90e543abbd616e4480e5fd3cfb9fac0764f
myCall <mycallthomas@gmail.com>
2019-01-30 15:26:44 +0000

    Added a Comment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 // - Write a function which receives an array of strings.
 // It should return the longest string from the array
+//ADD COMMENT
 function longestString(arr) {
   return arr.reduce(function(acc, item) {
     if (item.length > acc.length) {
